2e8b684485115318de7d6cf8609a2388ca5de07b
biztex <luxuryman887@gmail.com>
2022-11-04 01:06:02 +0900

    complete 5 days

--- 30-days-of-react/src/index.js
+++ 30-days-of-react/src/index.js
@@ -1,78 +1,14 @@
-// index.js
-import React from 'react'
-import ReactDOM from 'react-dom'
-import asabenehImage from './images/rose.jpg'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import avatar from "./images/rose.jpg";
+import reportWebVitals from "./reportWebVitals";
+import Apps from "./usercard";
 
-// Header Component
-const Header = () => (
-  <header>
-    <div className='header-wrapper'>
-      <h1>Welcome to 30 Days Of React</h1>
-      <h2>Getting Started React</h2>
-      <h3>JavaScript Library</h3>
-      <p>Asabeneh Yetayeh</p>
-      <small>Oct 3, 2020</small>
-    </div>
-  </header>
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Apps />
+  </React.StrictMode>
+);
 
-// User Card Component
-const UserCard = () => (
-  <div className='user-card'>
-    <img src={asabenehImage} alt='asabeneh' />
-    <h2>Asabeneh Yetayeh</h2>
-  </div>
-)
-
-// TechList Component
-const TechList = () => {
-  const techs = ['HTML', 'CSS', 'JavaScript']
-  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
-  return techsFormatted
-}
-
-
-const buttonStyles = {
-  padding: '10px 20px',
-  background: 'rgb(0, 255, 0',
-  border: 'none',
-  borderRadius: 5,
-}
-const Button = () => <button style={buttonStyles}> action </button>
-
-
-// Main Component
-const Main = () => (
-  <main>
-    <div className='main-wrapper'>
-      <p>Prerequisite to get started react.js:</p>
-      <ul>
-        <TechList />
-      </ul>
-      <UserCard />
-      <Button />
-    </div>
-  </main>
-)
-
-// Footer Component
-const Footer = () => (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>Copyright 2020</p>
-    </div>
-  </footer>
-)
-
-// The App, or the parent or the container component
-const App = () => (
-  <div className='app'>
-    <Header />
-    <Main />
-    <Footer />
-  </div>
-)
-
-const rootElement = document.getElementById('root')
-// we render the App component using the ReactDOM package
-ReactDOM.render(<App />, rootElement)
+reportWebVitals();
