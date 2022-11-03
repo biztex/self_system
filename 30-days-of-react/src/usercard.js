d5540e5bd98ec2943fdd04a9b093f2adf3196f95
biztex <luxuryman887@gmail.com>
2022-11-04 01:13:32 +0900

    modify usercard.js

--- 30-days-of-react/src/usercard.js
+++ 30-days-of-react/src/usercard.js
@@ -1,7 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import avatar from "./images/rose.jpg";
-import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 // Function to show month date year
