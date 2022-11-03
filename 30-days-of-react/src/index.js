5f4fc5270b790103e79bc6e291c4e53e2235015a
biztex <luxuryman887@gmail.com>
2022-11-04 01:07:07 +0900

    remove logo from index.js

--- 30-days-of-react/src/index.js
+++ 30-days-of-react/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import avatar from "./images/rose.jpg";
 import reportWebVitals from "./reportWebVitals";
 import Apps from "./usercard";
 
