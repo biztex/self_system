959dbac3e9e6cd9acba55f1d729d8c232f31f4b3
biztex <luxuryman887@gmail.com>
2022-12-10 11:34:05 +0900

    complete class component

--- 30-days-of-react/src/index.js
+++ 30-days-of-react/src/index.js
@@ -1,70 +1,188 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import myImage from "./images/rose.jpg";
 
-const numbers = [1, 2, 3, 4, 5];
-
-const skills = [
-  ["HTML", 10],
-  ["CSS", 10],
-  ["React", 2],
-];
-
-const countries = [
-  { name: "Finland", city: "Helsinki" },
-  { name: "Sweden", city: "Stockholm" },
-  { name: "Denmark", city: "Copenhagen" },
-  { name: "Norway", city: "Oslo" },
-  { name: "Iceland", city: "Reykjavik" },
-];
-
-// Number component
-const Numbers = ({ numbers }) => {
-  const list = numbers.map((number) => <li key={number}>{number}</li>);
-  return list;
-};
-// Skill Component
-const Skill = ({ skill: [tech, level] }) => (
-  <li>
-    {tech} {level}
-  </li>
+// User card component
+const UserCard = ({ user: { firstName, lastName, image } }) => (
+  <div className="user-card">
+    <img src={image} alt={firstName} />
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
+  </div>
 );
 
-// Skills Component
-const Skills = ({ skills }) => {
-  const skillsList = skills.map((skill) => <Skill key={skill} skill={skill} />);
-  console.log(skillsList);
-  return <ul>{skillsList}</ul>;
+// A button component
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+);
+
+// CSS styles in JavaScript Object
+const buttonStyles = {
+  backgroundColor: "#61dbfb",
+  padding: 10,
+  border: "none",
+  borderRadius: 5,
+  margin: 3,
+  cursor: "pointer",
+  fontSize: 18,
+  color: "white",
 };
 
-// Country component
-const Country = ({ country: { name, city } }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <small>{city}</small>
-    </div>
-  );
-};
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-// countries component
-const Countries = ({ countries }) => {
-  const countryList = countries.map((country) => (
-    <Country key={country.name} country={country} />
-  ));
-  return <div>{countryList}</div>;
-};
-const App = () => {
-  return (
-    <div className="container">
-      <h1>Mapping</h1>
-      <ul>
-        <Numbers numbers={numbers} />
-      </ul>
-      <Skills skills={skills} />
-      <Countries countries={countries} />
-    </div>
-  );
-};
+  render() {
+    console.log(this.props.data);
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data;
+
+    return (
+      <header>
+        <div className="header-wrapper">
+          <h1>{welcome}</h1>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <small>{date}</small>
+        </div>
+      </header>
+    );
+  }
+}
+
+class TechList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { techs } = this.props;
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
+    return techsFormatted;
+  }
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <main>
+        <div className="main-wrapper">
+          <p>Prerequisite to get started react.js</p>
+          <ul>
+            <TechList techs={this.props.techs} />
+          </ul>
+          <UserCard user={this.props.user} />
+          <Button
+            text="Greet People"
+            onClick={this.props.greetPeople}
+            style={buttonStyles}
+          />
+          <Button
+            text="Show Time"
+            onClick={this.props.handleTime}
+            style={buttonStyles}
+          />
+        </div>
+      </main>
+    );
+  }
+}
+
+// Footer Component
+// Class component
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <footer>
+        <div className="footer-wrapper">
+          <p>Copyright {this.props.date.getFullYear()}</p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+class App extends React.Component {
+  showDate = (time) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const month = months[time.getMonth()].slice(0, 3);
+    const year = time.getFullYear();
+    const date = time.getDate();
+    return `${month} ${date} ${year}`;
+  };
+
+  handleTime = () => {
+    alert(this.showDate(new Date()));
+  };
+
+  greetPeople = () => {
+    alert("Welcome to Class Component Challenge");
+  };
+
+  render() {
+    const data = {
+      welcome: "Welcome to 30 Days Of React",
+      title: "Getting Started React",
+      subtitle: "JavaScript Library",
+      author: {
+        firstName: "Asabeneh",
+        lastName: "Yetayeh",
+      },
+      date: "Dec 7, 2022",
+    };
+
+    const techs = ["HTML", "CSS", "JavaScript"];
+
+    const user = { ...data.author, image: myImage };
+
+    return (
+      <div className="App">
+        <Header data={data} />
+        <Main
+          user={user}
+          techs={techs}
+          handleTime={this.handleTime}
+          greetPeople={this.greetPeople}
+        />
+        <Footer date={new Date()} />
+      </div>
+    );
+  }
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
